b775576a084b7f3016f32840c00c1026467cbb71
withered_flower <2734850178@qq.com>
2026-08-15 19:05:22 +0800

    style: sort import

--- src/components/Ripple/index.tsx
+++ src/components/Ripple/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
-import styles from "./index.module.css"
 import { v4 } from "uuid"
+import styles from "./index.module.css"
 
 export default function Ripple() {
   const [ripples, setRipples] = useState<
