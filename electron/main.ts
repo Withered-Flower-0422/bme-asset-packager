c695092b6cbbe8d4d7f5b668edb98622d6bf4b59
withered_flower <2734850178@qq.com>
2026-08-16 01:36:50 +0800

    chore: bump version to 0.5.2

--- electron/main.ts
+++ electron/main.ts
@@ -4,27 +4,38 @@ import { fileURLToPath } from "url"
 import { store } from "./ipc/store"
 import "./ipc"
 
+const lock = app.requestSingleInstanceLock()
 const appDir = dirname(fileURLToPath(import.meta.url))
 
 const winSize = { width: 1000, height: 635 }
 
-app.whenReady().then(() => {
-  const win = new BrowserWindow({
-    ...winSize,
-    resizable: false,
-    title: "BME Asset Packager",
-    icon: join(appDir, "..", "public", "ballex2.ico"),
-    webPreferences: { preload: join(appDir, "preload.mjs") },
+let win: BrowserWindow
+if (!lock) app.quit()
+else {
+  app.on("second-instance", () => {
+    if (win.isMinimized()) win.restore()
+    win.show()
+    win.focus()
   })
-  win.removeMenu()
 
-  const bounds = store.get("winBounds")
-  if (bounds) win.setBounds({ ...bounds, ...winSize })
-  win.on("moved", () => store.set("winBounds", win.getBounds()))
+  app.whenReady().then(() => {
+    win = new BrowserWindow({
+      ...winSize,
+      resizable: false,
+      title: "BME Asset Packager",
+      icon: join(appDir, "..", "public", "ballex2.ico"),
+      webPreferences: { preload: join(appDir, "preload.mjs") },
+    })
+    win.removeMenu()
 
-  if (app.isPackaged) win.loadFile(join(appDir, "..", "dist", "index.html"))
-  else {
-    win.webContents.openDevTools({ mode: "detach" })
-    win.loadURL("http://localhost:5173")
-  }
-})
+    const bounds = store.get("winBounds")
+    if (bounds) win.setBounds({ ...bounds, ...winSize })
+    win.on("moved", () => store.set("winBounds", win.getBounds()))
+
+    if (app.isPackaged) win.loadFile(join(appDir, "..", "dist", "index.html"))
+    else {
+      win.webContents.openDevTools({ mode: "detach" })
+      win.loadURL("http://localhost:5173")
+    }
+  })
+}
